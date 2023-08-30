7627cc0e5f895ad1f09067107cc7d1151cb6009f
Arthur Lucas <123699745+ArthurSous4@users.noreply.github.com>
2023-08-29 21:27:11 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+} else {
+    console.log("dirija sempre usando sinto de segurança!")
+}
+
+
